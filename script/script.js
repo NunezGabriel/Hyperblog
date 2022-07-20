b2c7dd1f58aeada4538bd422146b02ba2bd6d39b
Gabriel Nunez <emilio.nunez.arenas@gmail.com>
2022-07-20 14:53:59 -0500

    cambios en branch

--- script/script.js
+++ script/script.js
@@ -1,1 +1,11 @@
-document.write('aca abajo info maquina')
+document.write('aca abajo info maquina');
+
+for(i=0; i<10; i++)
+{
+    document.write(i)
+}
+
+if (i = 5)
+{
+    document.write("hola")
+}
